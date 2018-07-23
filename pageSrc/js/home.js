17baeb7625e855836c462b30ff619e71c7705561
xunxingyuan <xunxingyuan@gmail.com>
2018-07-23 22:40:54 +0800

    add js

--- pageSrc/js/home.js
+++ pageSrc/js/home.js
@@ -1,30 +1,13 @@
-(function () {
-
-})()
-
+var lists = []
 $(document).ready(function(){
-	var lists = [
-		{
-			"id": "1531992037697_2415",
-			"author": "xun",
-			"title": "测试分享",
-			"preview": "这是一个关于内容的部分",
-			"type": 0,
-			"price": 20,
-			"modify": 1531992037697,
-			"count": 0
-		},
-		{
-			"id": "1531992054917_6569",
-			"author": "xun",
-			"title": "测试分享",
-			"preview": "这是一个关于内容的部分",
-			"type": 0,
-			"price": 20,
-			"modify": 1531992054917,
-			"count": 0
+
+	axios.get('/api/public/query').then(function (res) {
+		if(res.data.code === 200){
+			res.data.data.lists.forEach(function (value) {
+				value.modify = new Date(value.modify).toLocaleString()
+			})
+			lists = res.data.data.lists
+			$('#question').tmpl(lists).appendTo("#content")
 		}
-	]
-	console.log($('#question'))
-	$('#question').tmpl(lists).appendTo("#content")
+	})
 })
